2d279c02290c342f36c2e321067ba092f242a1d3
David Stern <Home@resnet136-10.medford.tufts.edu>
2016-02-14 21:59:27 -0500

    got rid of body fieldname

--- route/route_handler.js
+++ route/route_handler.js
@@ -13,7 +13,6 @@ var mongoose = require ('mongoose');
 var mongo = require('mongodb');
 var mongoskin = require('mongoskin');
 var ObjectId = mongo.ObjectId;
-var BSON = mongo.BSONPure;
 var app = express();
 
 /* Connect to database */
@@ -37,7 +36,7 @@ exports.getById = function(req, res) {
 	Collection.findOne({'_id':new ObjectId(id) }, function(err, result){
     
     if (err) 
-    	return next(err)
+    	throw err
     res.send(result)
   })
 };
@@ -47,47 +46,45 @@ exports.getAll = function(req, res) {
 	Collection.find().toArray(function(err, results){
 	
 	if (err) 
-		return next(err)
+		throw err
     res.send(results)
 	})
 };
 
 /* Post Object */
 exports.post = function(req, res) {
-	var body = req.body
-	Collection.insert({body}, {}, function(err, results) {
-		
+	Collection.insert(req.body, {}, function(err, results) {
 	if (err)
-		return next(err)
+		throw err
 	res.send(results.ops)
 	})
+}
 
-};
 /* Update Object */
 exports.put = function(req, res) {
 	var id = req.params.id;
 	var body = req.body;
-	Collection.remove({'_id':new ObjectId(id) }, function(err, result){
-    if (err) 
-    	return next(err)
-    })
 
-	Collection.insert({'_id':new ObjectId(id), body }, {}, function(err, result){
+    Collection.update({'_id': new ObjectId(id)}, {$set:req.body}, {multi:false}, function(err, results){
+   	 if (err) 
+    	throw err
+
+  	  Collection.findOne({'_id':new ObjectId(id) }, function(errs, result){
     
-    if (err) 
-    	return next(err)
-
-    res.send(result.ops)
-	})
+  	  if (errs) 
+ 		   	throw errs
+  	  res.send(result)
+  	  })
+  	})
+	
 };
 
 /* Remove Object */
 exports.delete = function(req, res) {
 	var id = req.params.id
-    console.log('Deleting Objects: ' + id)
     Collection.remove({'_id':new ObjectId(id) }, function(err, result){
     if (err) 
-    	return next(err)
+    	throw err
     })
     res.send()
 };
